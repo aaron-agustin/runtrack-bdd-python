36a6957488a372384eca441e6938858e17c432b4
aaron-agustin <aaron.agustin@laplateforme.io>
2024-01-30 18:52:28 +0100

    fait

---
+++ Jour02/job04.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+# Se connecter à la base de données
+conn = mysql.connector.connect(
+    host="localhost",
+    user="Aaron",
+    password="Aaron07!",
+    database="LaPlateforme"
+)
+
+# Créer un curseur pour exécuter des requêtes SQL
+cursor = conn.cursor()
+
+# Exécuter la requête pour récupérer les noms et les capacités des salles
+cursor.execute("SELECT nom, capacite FROM salle")
+
+# Récupérer tous les résultats
+salles = cursor.fetchall()
+
+# Afficher les résultats
+print(salles)
+
+# Fermer le curseur et la connexion
+cursor.close()
+conn.close()
+
